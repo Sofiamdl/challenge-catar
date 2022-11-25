11eb6bc835311f93317695ec927ea266a4ae9f73
Sofia Melo <sofianovica@hotmail.com>
2022-11-25 14:47:28 -0300

    feature(add-base): create watch project

---
+++ challenge-catar Watch App/challenge_catarApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_catarApp.swift
+//  challenge-catar Watch App
+//
+//  Created by sml on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct challenge_catar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
