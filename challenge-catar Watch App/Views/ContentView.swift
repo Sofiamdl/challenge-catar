c5e13001616a55c45d915d2eb070f58d911edf37
Alex Damascena <apfd@cin.ufpe.br>
2022-11-27 02:03:04 -0300

    feature(today-avarage-component): create base views and structs

--- challenge-catar Watch App/Views/ContentView.swift
+++ challenge-catar Watch App/Views/ContentView.swift
@@ -15,44 +15,101 @@ struct Data: Identifiable {
     var value:Int
 }
 
+extension WKInterfaceDevice {
+    
+    static var size: CGRect {
+        return WKInterfaceDevice.current().screenBounds
+    }
+}
+
+enum LineOrientation {
+    
+    case vertical
+    case horizontal
+        
+    var size: CGSize {
+        switch self {
+        case .horizontal:
+            return CGSize(width: WKInterfaceDevice.size.width, height: 1)
+        case .vertical:
+            return CGSize(width: 3, height: 19)
+        }
+    }
+}
+
+struct Line: View {
+
+    let orienttion: LineOrientation
+    let withColor: Color
+    
+    var body: some View {
+        Rectangle()
+            .fill(withColor)
+            .frame(width: orienttion.size.width,
+                   height: orienttion.size.height,
+                   alignment: .center)
+            .cornerRadius(2)
+    }
+}
+
 struct ContentView: View {
     @EnvironmentObject private var coordinator: Coordinator
 
-    let data = [Data(id: UUID().uuidString, time: 2, value: 20),
-                Data(id: UUID().uuidString, time: 1, value: 10),
-                Data(id: UUID().uuidString, time: 2, value: 120),
-                Data(id: UUID().uuidString, time: 3, value: 30),
-                Data(id: UUID().uuidString, time: 2, value: 20)]
+//    let data = [Data(id: UUID().uuidString, time: 2, value: 20),
+//                Data(id: UUID().uuidString, time: 1, value: 10),
+//                Data(id: UUID().uuidString, time: 2, value: 120),
+//                Data(id: UUID().uuidString, time: 3, value: 30),
+//                Data(id: UUID().uuidString, time: 2, value: 20)]
+    
     var body: some View {
-        HStack{
-            Chart {
-                
-                ForEach(data) { temp in
-                    
-                    LineMark(
-                        
-                        x: .value("Time", temp.time),
-                        
-                        y: .value("Temp", temp.value)
-                        
-                    )
-                    
-                }
-                
+        
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Hoje")
+            HStack(spacing: 32){
+                Text("Tempo")
+                Text("Qualidade")
             }
             
-            .frame(height: 200)
-            .padding()
-        }
-        if #available(iOS 16.0, *) {
-            Button("Oi") {
-                coordinator.goToSomeView()
+            HStack(spacing: 24) {
+                Text("5 horas")
+                Line(orienttion: .vertical, withColor: .blue)
+                
+            
+                Text("80%")
             }
-        } else {
-            NavigationLink(destination: SomeView()) {
-                Text("Oi")
-            }
+                    
         }
+        
+
+//        HStack{
+//            Chart {
+//
+//                ForEach(data) { temp in
+//
+//                    LineMark(
+//
+//                        x: .value("Time", temp.time),
+//
+//                        y: .value("Temp", temp.value)
+//
+//                    )
+//
+//                }
+//
+//            }
+//
+//            .frame(height: 200)
+//            .padding()
+//        }
+//        if #available(iOS 16.0, *) {
+//            Button("Oi") {
+//                coordinator.goToSomeView()
+//            }
+//        } else {
+//            NavigationLink(destination: SomeView()) {
+//                Text("Oi")
+//            }
+//        }
 
     }
 }
