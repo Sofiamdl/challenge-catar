2562887c358d0d358cded873b0795651fd8f9773
Alex Damascena <apfd@cin.ufpe.br>
2022-11-27 15:22:59 -0300

    feature(today-avarage-component): card information component done, needrefactoring

--- challenge-catar Watch App/Views/ContentView.swift
+++ challenge-catar Watch App/Views/ContentView.swift
@@ -96,22 +96,42 @@ enum IconStatus: String {
     case withoutIcon
 }
 
+enum TitleCardInformation: String {
+    case today = "Hoje"
+    case avarage = "Média Semanal"
+}
+
 struct ColorConstant {
     static let BLUE = "blue"
     static let LIGHT_GRAY = "light_gray"
+    static let PURPLE = "purple"
+}
+
+enum Screen {
+    case running
+    case sleep
+}
+
+struct CardValues {
+    let leftSideContent: String
+    let rightSideContent: String
 }
 
 struct CardInformation: View {
+    
+    let values: CardValues
     let iconStatus: IconStatus
+    let title: TitleCardInformation
+    let page: Screen
     
     let insets = EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0)
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 6) {
+        VStack(alignment: .leading, spacing: 7) {
             
             HStack(spacing: 8){
-                TextView(text: "Hoje",
-                         color: Color(ColorConstant.BLUE),
+                TextView(text: title.rawValue,
+                         color: Color(page == .sleep ? ColorConstant.PURPLE : ColorConstant.BLUE),
                          type: .title)
                 .padding(insets)
                 
@@ -124,31 +144,31 @@ struct CardInformation: View {
                 }
             }
                         
-            HStack(spacing: 56){
-                TextView(text: "Tempo",
+            HStack(spacing: page == .sleep ? 56 : 40){
+                TextView(text: page == .sleep ? "Tempo" : "Distância",
                          color: Color(ColorConstant.LIGHT_GRAY),
                          type: .description)
-                TextView(text: "Qualidade",
+                TextView(text: page == .sleep ? "Qualidade" : "Velocidade",
                          color: Color(ColorConstant.LIGHT_GRAY),
                          type: .description)
             }
             .padding(insets)
             
-            HStack(spacing: 18) {
-                TextView(text: "5 horas",
+            HStack(spacing: page == .sleep ? 18 : 22) {
+                TextView(text: values.leftSideContent,
                          color: .white,
                          type: .value)
                 Line(orienttion: .vertical,
-                     withColor: Color(ColorConstant.BLUE))
+                     withColor: Color(page == .sleep ? ColorConstant.PURPLE : ColorConstant.BLUE))
                 
-                TextView(text: "80%",
+                TextView(text: values.rightSideContent,
                          color: .white,
                          type: .value)
             }
             .padding(insets)
             
             Line(orienttion: .horizontal,
-                 withColor: Color(ColorConstant.BLUE))
+                 withColor: Color(page == .sleep ? ColorConstant.PURPLE : ColorConstant.BLUE))
         }
     }
 }
@@ -158,7 +178,11 @@ struct ContentView: View {
     @EnvironmentObject private var coordinator: Coordinator
 
     var body: some View {
-        CardInformation(iconStatus: .withoutIcon)
+        let cardContent = CardValues(leftSideContent: "5 horas", rightSideContent: "80%")
+        CardInformation(values: cardContent,
+                        iconStatus: .withoutIcon,
+                        title: .today,
+                        page: .sleep)
     }
 }
 
