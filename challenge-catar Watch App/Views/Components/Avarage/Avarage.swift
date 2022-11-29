adbf7349e7a7ae9fbc7dba0c298ef5f25e26ecf7
Alex Damascena <apfd@cin.ufpe.br>
2022-11-29 14:45:28 -0300

    feature(avarage-component): create avarage component

---
+++ challenge-catar Watch App/Views/Components/Avarage/Avarage.swift
@@ -0,0 +1,50 @@
+//
+//  Avarage.swift
+//  challenge-catar Watch App
+//
+//  Created by alexdamascena on 29/11/22.
+//
+
+import SwiftUI
+
+struct Avarage: View {
+    
+    let distancePerHour: String
+    let sleepingHours: String
+    
+    private var contentView: some View {
+        HStack(spacing: 12) {
+            createIcon(withImage: "figure.run")
+            TextView(text: distancePerHour,
+                     color: .white,
+                     type: .value)
+            Line(orienttion: .vertical,
+                 withColor: Color(ColorConstant.PURPLE))
+            createIcon(withImage: "bed.double.fill")
+            TextView(text: sleepingHours,
+                     color: .white,
+                     type: .value)
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8){
+            TextView(text: "Médias",
+                     color: Color(ColorConstant.LIGHT_GRAY),
+                     type: .description)
+            contentView
+        }
+    }
+    
+    func createIcon(withImage image: String) -> some View {
+        Image(systemName: image)
+            .foregroundColor(Color(ColorConstant.PURPLE))
+            .font(.system(size: 20, weight: .bold))
+    }
+}
+
+struct Avarage_Previews: PreviewProvider {
+    static var previews: some View {
+        Avarage(distancePerHour: "19 km/h", sleepingHours: "5h")
+    }
+}
