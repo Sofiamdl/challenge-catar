4c7c98e8dd0bd698ec4a9e9f5d6a2e251afb7725
Sofia Melo <sofianovica@hotmail.com>
2022-11-29 15:54:04 -0300

    feature(graph-sleep): finish base of graph

--- challenge-catar Watch App/challenge_catarApp.swift
+++ challenge-catar Watch App/challenge_catarApp.swift
@@ -9,17 +9,11 @@ import SwiftUI
 
 @main
 struct challenge_catar_Watch_AppApp: App {
-    @ObservedObject var coordinator = Coordinator()
 
     var body: some Scene {
         WindowGroup {
-            NavigationStack(path: $coordinator.path) {
                 AppCore()
-                .navigationDestination(for: Destination.self) { destination in
-                    ViewFactory.viewForDestination(destination)
-                }
-            }
-            .environmentObject(coordinator)
+
         }
     }
 }
