3c584a96cb180169a7c33038cb510263d813f5cf
Alex Damascena <apfd@cin.ufpe.br>
2022-11-30 03:49:06 -0300

    feature(racing-screen): running screen completed

--- challenge-catar Watch App/Views/Core/RunningScreen.swift
+++ challenge-catar Watch App/Views/Core/RunningScreen.swift
@@ -8,31 +8,37 @@
 import SwiftUI
 
 struct RunningScreen: View {
+    
+    let todayDistanceVelocityValues = CardValues(leftSideContent: "36 km",
+                                                 rightSideContent: "18 km/h")
+    
+    let avarageDistanceVelocityValues = CardValues(leftSideContent: "36 km",
+                                                   rightSideContent: "18 km/h")
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .center, spacing: 8){
-                let todayDistanceVelocityValues = CardValues(leftSideContent: "36 km", rightSideContent: "18 km/h")
                 
                 CardInformation(values: todayDistanceVelocityValues,
                                 iconStatus: .withoutIcon,
                                 title: .today, page: .running)
-                
-                let avarageDistanceVelocityValues = CardValues(leftSideContent: "36 km",
-                                                               rightSideContent: "18 km/h")
                 
                 CardInformation(values: avarageDistanceVelocityValues,
                                 iconStatus: .increasing,
                                 title: .avarage,
                                 page: .running)
                 
-                DailyProgressGraphicView(values: [ 1,2,3,4,5,6,7],
-                                         labels: ["S", "T", "Q", "Q", "S", "S", "D"] )
+                DailyProgressGraphicView(values: [1,2,3,4,5,6,7],
+                                         labels: ["S", "T", "Q", "Q", "S", "S", "D"],
+                                         maxValue: 7,
+                                         screen: .runningScreen)
             }
             .padding()
-            
         }
+        .navigationBarTitle("corrida")
     }
 }
+
 
 struct RunningScreen_Previews: PreviewProvider {
     static var previews: some View {
