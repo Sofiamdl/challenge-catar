672ed030068a1b0029c69ac916aeac1e8e13c963
Alex Damascena <apfd@cin.ufpe.br>
2022-12-06 13:24:37 -0300

    feature(refactoring): refactoing sleep analysis

---
+++ challenge-catar Watch App/Typealias/Sleep.swift
@@ -0,0 +1,16 @@
+//
+//  Sleep.swift
+//  challenge-catar Watch App
+//
+//  Created by alexdamascena on 06/12/22.
+//
+
+import Foundation
+
+
+typealias SleepyDay = String
+typealias TypeSleepAndTime = [(Double, Int)]
+typealias SleepDataCollection = [SleepyDay : TypeSleepAndTime]
+typealias SleepData = (Double, Int)
+
+typealias Intervals = (TimeInterval, TimeInterval, TimeInterval)
